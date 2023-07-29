34ad92e78d68461adca5a0943d6fa63ce713de9f
@32466 <daniel_hegenbart@yahoo.de>
2023-07-29 07:23:46 +0200

    Update App.js

--- beetappstyle/src/App.js
+++ beetappstyle/src/App.js
@@ -1,31 +1,25 @@
 import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import { useState, useEffect } from "react";
 import { PropData } from "./index";
-import { ThemeProvider } from "styled-components";
 import "./App.css";
-import { useTheme } from "./customhooks/useTheme";
 import HomeNotClicked from "./components/HomeNotClicked";
-import Layout from "./pages/Layout";
-import Overview from "./pages/Overview";
-import Intro from "./pages/Intro";
 import Homepage from "./pages/Homepage";
 
 
 
 
-/**
-
- */
-
-
-
-
-
-  //simple routing
-  //for more information see https://reactrouter.com/en/main/start/tutorial
- 
-    
-      
+class App extends Component{
+  render(){
+      return(
+          <Router>
+              <Switch>               
+                  <Route exact path="/" component={Homepage}/>
+              </Switch>
+          </Router>
+      ) 
+   }
+}
 
 
 
