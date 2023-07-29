733dff35b522ba126dd0c605f5faa972e0fe29c2
@32466 <daniel_hegenbart@yahoo.de>
2023-07-29 07:50:11 +0200

    Update App.js

--- beetappstyle/src/App.js
+++ beetappstyle/src/App.js
@@ -1,24 +1,26 @@
 import React, { Component } from 'react' ;
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Routes} from "react-router-dom";
 import "./App.css";
 import Homepage from "./pages/Homepage";
+import SucheAngeklickt from "./pages/SucheAngeklickt;
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div className="App">
-          <Route path='/' component={Homepage} />
-        </div>
-      </Router>
-    );
-  }
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />}>
+            <Route path="SucheAngeklickt" element={<SucheAngeklickt />} />
+            
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-
-
-
-
 export default App;
-
 
